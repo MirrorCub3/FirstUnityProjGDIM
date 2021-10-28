2787c32bd7bc5b70db477649010d7f288c9d339c
MirrorCube <joycemai.exe@gmail.com>
2021-10-28 00:13:24 -0700

    Changed PlayerScript
    Made it so the jittering is less apparent when colliding with borders

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class PlayerScript : MonoBehaviour
 {
     public float speed = 5f;
+    private float xtrans;
     public float jumpForce = 20f;
     public float floatForce = 6f;
     public bool isJumping = false;
@@ -22,15 +23,14 @@ public class PlayerScript : MonoBehaviour
         wings = false;
         anim = this.GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
-        
+
         faceRight = true;
         isJumping = false;
     }
 
     void Update()
     {
-
-        float xtrans = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        xtrans = Input.GetAxis("Horizontal") * speed;
         if (xtrans > 0) // determines which way the player is facing
         {
             faceRight = true;
@@ -40,27 +40,16 @@ public class PlayerScript : MonoBehaviour
             faceRight = false;
         }
 
-        transform.Translate(xtrans, 0, 0);
-        if (transform.position.x > GeneralScript.right)
-        {
-            transform.position = new Vector3(GeneralScript.right, transform.position.y, transform.position.z);
-        }
-        else if (transform.position.x < GeneralScript.left)
-        {
-            transform.position = new Vector3(GeneralScript.left, transform.position.y, transform.position.z);
-        }
-
         isJumping = false;
 
         if (Input.GetButtonDown("Jump") && (Mathf.Abs(myRigidbody.velocity.y) < 0.001f || wings)) // only allows jumping if not already up or if you have wings
         {
-            if(wings)
+            if (wings)
                 myRigidbody.AddForce(new Vector2(0f, floatForce), ForceMode2D.Impulse);
             else
                 myRigidbody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
             isJumping = true;
         }
-
         anim.SetFloat("Speed", xtrans);
         anim.SetBool("FaceRight", faceRight);
     }
@@ -70,5 +59,8 @@ public class PlayerScript : MonoBehaviour
         wings = true;
         anim.SetTrigger("Wings");
     }
-
+    private void FixedUpdate()
+    {
+        transform.Translate(xtrans * Time.fixedDeltaTime, 0,0);
+    }
 }
